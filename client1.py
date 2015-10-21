4fb53169e3b516df1e6a5585787775e0e6508f4e
Paul G Mathew <paulgmathew@gmail.com>
2015-10-21 13:58:03 -0500

    Update client1.py

--- client1.py
+++ client1.py
@@ -1,19 +1,22 @@
 import socket
-import threading
-import socket
-import time
-import hashlib
-import json
-import random
+import sys
 
-TCP_IP = '127.0.0.1'
-TCP_PORT = 11500
-BUFFER_SIZE = 1024
-
-def connect():
-	sock = socket.socket()
-	sock.connect((TCP_IP, port))
-	sock.listen(1)
-	while 1:
-		print sock.recv(1024)	
-connect()
+# create a TCP/IP socket 
+sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server_address = ('23.251.157.195',11500)
+print >> sys.stderr,'connecting to %s port %s'%server_address
+sock.connect(server_address)
+try:
+	#send data 
+	message = "this is the message and it will be repeated"
+	print >> sys.stderr,'sending"%s"'% message
+	sock.sendall(message)
+	amount_received = 0 
+	amount_expected = len(message)	
+	while amount_received < amount_expected:
+		data = sock.recv(16)
+		amount_received += len(data)
+		print >> sys.stderr,'received "%s"' % data
+finally: 
+	print >> sys.stderr,'closing socket'
+	sock.close()
